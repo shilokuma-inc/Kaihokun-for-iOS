32ededb9d85c5a1c0537d54c34617da0bd1e2c2a
mrs1669 <mrs1669.develop@gmail.com>
2020-04-20 13:24:01 +0900

    new create 開法君

---
+++ Kaihokun/Kaihokun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kaihokun
+//
+//  Created by Takumi Muraishi on 2020/04/20.
+//  Copyright © 2020 Takumi Muraishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
