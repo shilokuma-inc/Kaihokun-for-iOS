950dbf5daf33c4f4c2386cef9c77160cb2a9f1cd
mrs1669 <mrs1669.develop@gmail.com>
2020-04-23 23:55:16 +0900

    set startButton Position

--- Kaihokun/Kaihokun/ViewController.swift
+++ Kaihokun/Kaihokun/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
         titleLabel.frame = CGRect(x: 0, y: UIScreen.main.bounds.size.height/4, width: UIScreen.main.bounds.size.width, height: 100) // 上から1/4、中心にタイトルを指定
         titleLabel.textAlignment = NSTextAlignment.center // set titleLabel textalign center
         titleLabel.font = UIFont(name:"HiraMaruProN-W4",size:60) // set ヒラギノ丸
+        
+        startButton.frame = CGRect(x: 0, y: UIScreen.main.bounds.size.height*3/5, width: UIScreen.main.bounds.size.width, height: 100) // 下から1/4、中心にタイトルを指定
     }
 
 
